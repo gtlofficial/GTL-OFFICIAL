8183a870233c9d4d689ac968228f9be7d719f5f7
nehagit0730 <nehadice2020@gmail.com>
2025-05-22 16:07:30 +0530

    making work page

---
+++ app/(innerPages)/page-work/page.jsx
@@ -0,0 +1,31 @@
+import Header2 from "@/components/headers/Header2";
+
+import Footer2 from "@/components/footers/Footer2";
+
+import Faq from "@/components/homes/home-2/Faq";
+import Testimonials from "@/components/homes/home-3/Testimonials";
+import Cta from "@/components/innerpages/features/Cta";
+import FilterGallery from "@/components/homes/home-1/FilterGallery";
+export const metadata = {
+  title:
+    "Work || GTL - Full-featured, professional-looking software, saas and startup nextjs template.",
+  description:
+    "GTL - Full-featured, professional-looking software, saas and startup nextjs template.",
+};
+export default function WorkPage() {
+  return (
+    <>
+      <div className="page-wrapper uni-body panel bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-200 overflow-x-hidden bp-xs bp-sm bp-md bp-lg bp-xl bp-xxl dom-ready">
+        <Header2 />
+        <div id="wrapper" className="wrap">
+          <FilterGallery />
+          {/* <div className="pt-6 xl:pt-9"></div> */}
+          <Faq />
+          <Testimonials />
+          <Cta />
+        </div>
+        <Footer2 />
+      </div>
+    </>
+  );
+}
