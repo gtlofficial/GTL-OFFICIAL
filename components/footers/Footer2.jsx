eb4aa4620fb0c804b933a8667dc9eed24d055085
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2025-06-11 14:12:39 +0530

    Update Footer2.jsx

--- components/footers/Footer2.jsx
+++ components/footers/Footer2.jsx
@@ -52,7 +52,7 @@ export default function Footer2() {
                 <div className="col-12 lg:col-3">
                   <div className="panel vstack items-start gap-1">
                     <p className="opacity-80">
-                     Galactic Technologies is a leading IT company founded in 2015, offering custom website, app, theme, and full-stack development services to help your business grow online. To empower businesses and startups by turning ideas into reality with innovative, efficient, and future-ready digital solutions.
+                     GTL Web Solutions is a leading IT company founded in 2015, offering custom website, app, theme, and full-stack development services to help your business grow online. To empower businesses and startups by turning ideas into reality with innovative, efficient, and future-ready digital solutions.
                     </p>
                     <CalendlyButton
                 className="btn btn-sm btn-primary text-white text-none d-none lg:d-inline-flex"
