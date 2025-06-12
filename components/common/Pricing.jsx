b1544759521e5cce141122e8f0d6518167377aa7
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2025-06-12 16:15:13 +0530

    Update Pricing.jsx

--- components/common/Pricing.jsx
+++ components/common/Pricing.jsx
@@ -74,7 +74,7 @@ export default function Pricing() {
                             <span className="fs-6 fw-bold dark:text-white">
                               {tier.title === "Essentials"
                                 ? "Key features:"
-                                : `Services Include: 
+                                : `Services Include:  plus:`}
                             </span>
                             {tier.features.map((feature, idx) => (
                               <div key={idx} className="hstack gap-1 fs-7">
